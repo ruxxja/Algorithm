5b9fa0232a051644be235a6f92baa401649df1f0
Kyungbeom Kim <166625539+ruxxja@users.noreply.github.com>
2025-07-09 01:55:56 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    int N;
+    scanf("%d", &N);
+    for (int y = 0; y < N; y++)
+    {
+        for (int x = y; x < N - 1; x++)
+            printf(" ");
+        for (int x = 0; x < (y + 1) * 2 - 1 ; x++)
+            printf("*");
+
+        printf("\n");
+    }
+    for (int y = 0; y < N - 1; y++)
+    {
+        for (int x = 0; x < y + 1; x++)
+            printf(" ");
+        for (int x = 0; x < N * 2 - (y + 1) * 2 - 1; x++)
+            printf("*");
+        printf("\n");
+    }
+
+    return 0;
+}
